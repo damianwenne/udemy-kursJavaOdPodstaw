fd0b935a8d1e9c38f5d0de5a5e2245a4752974c6
damianwenne <damianwenne@gmail.com>
2020-05-02 16:20:52 +0200

    Dodanie typu prostego byte

--- src/main/java/com/udemy/sekcja3_zmienne/PrimitiveTypes.java
+++ src/main/java/com/udemy/sekcja3_zmienne/PrimitiveTypes.java
@@ -1,4 +1,10 @@
 package com.udemy.sekcja3_zmienne;
 
 public class PrimitiveTypes {
+    public static void main(String[] args) {
+
+        byte zmiennaByte = 10;
+        System.out.println("Zmienna Byte: " + zmiennaByte);
+
+    }
 }
