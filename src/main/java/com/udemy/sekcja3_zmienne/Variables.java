8355c08fc83828698affc984b73cf2343a8e826c
damianwenne <damianwenne@gmail.com>
2020-05-02 16:11:18 +0200

    Uzupelnienie metody

--- src/main/java/com/udemy/sekcja3_zmienne/Variables.java
+++ src/main/java/com/udemy/sekcja3_zmienne/Variables.java
@@ -3,5 +3,21 @@ package com.udemy.sekcja3_zmienne;
 public class Variables {
     public static void main(String[] args) {
 
+        int dowolnaNazwaDlaInteger;
+
+        int liczbaOkienWDomku; // deklaracja zmiennej
+        liczbaOkienWDomku = 30; // przypisanie wartości do zmienne
+        liczbaOkienWDomku = 32; // modyfikacja zmiennej
+
+        int liczbaDrzwiWDomku = 10; // inicjalizacja zmiennej
+
+        int zmiennaDlaLiczbCalkowitych = 111;
+
+        double zmiennaDlaLiczbZmiennoprzecinkowych = 114.64;
+
+        int sumaOtworowWDomku = liczbaDrzwiWDomku + liczbaOkienWDomku;
+        int sumaKolejna = zmiennaDlaLiczbCalkowitych + liczbaDrzwiWDomku;
+
+
     }
 }
